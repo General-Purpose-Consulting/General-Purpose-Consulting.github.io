ca0cc4b6c3f5721914b4bf5c1e6508b0485cde31
Alex Serriere <alex.serriere@teecom.com>
2015-09-12 00:27:24 -0700

    First pass at randomization features

---
+++ js/gpc_main.js
@@ -0,0 +1,46 @@
+// Handles javascript for main GPC site
+//
+/* Discipline randomizer */
+//
+var disciplines = [
+  'Chemistry',
+  'Biology',
+  'Engineering',
+  'Planning',
+  'Construction',
+  'Science',
+  'Music'
+];
+
+var modifiers = [
+  'Rocket',
+  'Behavioral',
+  'Neurological',
+  'Planetary',
+  'Botanical',
+  'Genetic'
+];
+
+function getRandomDiscipline() {
+  var maxDisciplines = disciplines.length;
+  var maxModifiers = modifiers.length;
+
+  var disciplineIndex = Math.floor(Math.random() * maxDisciplines);
+  var modifierIndex = Math.floor(Math.random() * maxModifiers);
+
+  var modifiedDiscipline = modifiers[modifierIndex] + " " + disciplines[disciplineIndex];
+
+  modifiers.splice(modifierIndex,1);
+  disciplines.splice(disciplineIndex,1);
+
+  return modifiedDiscipline;
+
+}
+
+$( document ).ready(function() {
+
+    $(".discipline").each(function() {
+      $(this).text(getRandomDiscipline());
+    });
+
+});
